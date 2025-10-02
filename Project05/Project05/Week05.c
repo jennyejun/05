3313d213de0e1de7168903d2e3d83b908ed5f34a
jennyejun <aprilhan@sookmyung.ac.kr>
2025-10-02 15:20:53 +0900

    Update Week05.c

--- Project05/Project05/Week05.c
+++ Project05/Project05/Week05.c
@@ -5,7 +5,6 @@ void main() {
 	printf("정수 하나를 입력하시오 : ");
 	scanf_s("%d", &myNum);
 
-	if (myNum > 0) printf("양수입니다.");
-	else if (myNum < 0) printf("음수입니다.");
-	else printf("0 입니다.");
+	if (myNum >= 0) printf("절대값은 %d 입니다.", myNum);
+	else printf("절대값은 %d 입니다.", -myNum);
 }
