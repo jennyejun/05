e3a6062a3a526003923c7e0ba6c78c615f5d59d1
jennyejun <aprilhan@sookmyung.ac.kr>
2025-10-02 15:58:54 +0900

    Update Week05.c

--- Project05/Project05/Week05.c
+++ Project05/Project05/Week05.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
 void main() {
-	int n, i;
-	int sum = 0;
+	int a = 0, b = 0, c;
+	char calc;
 
-	printf("input a number:");
-	scanf_s("%d", &n);
+	printf("input the calculation : ");
+	scanf_s("%d %c %d", &a, &calc, 1, &b);
 
-	for (i = 1; i <= n; i++) sum = sum + i;
-	printf("The result is %d", sum);
+	if (calc == '+') c = a + b;
+	else if (calc == '-') c = a - b;
+	else if (calc == '*') c = a * b;
+	else c = a / b;
+
+	printf("= %d", c);
 }
